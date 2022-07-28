cb97e976d36ef65ad20b4e57b9d594b4631fa66e
Asael <zanduva@gmail.com>
2022-07-27 23:49:45 -0500

    esta es una segunda prueba

---
+++ prueba.js
@@ -0,0 +1,10 @@
+{
+    "lessonTitle": "Primeros Pasos",
+    "feedbackID": "00-PrimerosPasos",
+    "permalink": "/Primeros_Pasos/",
+    "eleventyNavigation": {
+      "key": "Primeros Pasos",
+      "order": 2
+    }
+  }
+  
